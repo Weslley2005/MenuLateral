2ca5c70b4fece5924645bb31edf87fb27daaee25
Andre <andre.martins@unigran.br>
2023-11-10 23:43:00 -0400

    final

--- app/src/main/java/br/unigran/menulateral/MainActivity.java
+++ app/src/main/java/br/unigran/menulateral/MainActivity.java
@@ -11,10 +11,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,17 +26,55 @@ public class MainActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
     NavigationView navigationView;
+    ImageView img ;
+    FloatingActionButton fb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fb=findViewById(R.id.idfb);
         drawerLayout=findViewById(R.id.drawerLayout);
         navigationView= findViewById(R.id.navView);
+        img = findViewById(R.id.imgId);
+
         toggle= new ActionBarDrawerToggle(this,drawerLayout,R.string.fechar,R.string.abrir);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        fb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(toggle.onOptionsItemSelected(item))
+            return true;
+        return super.onOptionsItemSelected(item);
+    }
+    public void acao(View v){
+        Intent it = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        someActivityResultLauncher.launch(it);
 
     }
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Here, no request code
+                        Intent data = result.getData();
+                        Bundle extras = data.getExtras();
+                        Bitmap imagem = (Bitmap) extras.get("data");
+
+                        img.setImageBitmap(imagem);
+                    }
+                }
+            });
 }
